002cd645276e5ff958d9e387625dcbb933a7f431
mxhbmnn <maximilian.hubmann@mailbox.tu-dresden.de>
2025-07-10 12:39:00 +0200

    Removed old parameters in ContainerLoadingParams

--- cpp/3L-VehicleRouting/ContainerLoading/include/ContainerLoading/ProblemParameters.h
+++ cpp/3L-VehicleRouting/ContainerLoading/include/ContainerLoading/ProblemParameters.h
@@ -149,40 +149,11 @@ struct LoadingProblemParams
     };
 };
 
-struct BranchAndCutParameters
-{
-    enum class CallType
-    {
-        None,
-        Exact,
-        ExactLimit,
-        Heuristic
-    };
-
-    std::unordered_map<CallType, double> TimeLimits = {
-        {CallType::Exact, std::numeric_limits<double>::max()},
-        {CallType::ExactLimit, 1.0},
-        {CallType::Heuristic, 0.0},
-    };
-};
-
 struct ContainerLoadingParams
 {
     CPSolverParams CPSolver;
     LoadingProblemParams LoadingProblem;
     ClassifierParams classifierParams;
-    BranchAndCutParameters BranchAndCut;
-
-   [[nodiscard]] double DetermineMaxRuntime(BranchAndCutParameters::CallType callType,
-                               double residualTime = std::numeric_limits<double>::max()) const
-    {
-        return std::min(BranchAndCut.TimeLimits.at(callType), residualTime);
-    }
-
-   [[nodiscard]] bool IsExact(BranchAndCutParameters::CallType callType) const
-    {
-        return callType == BranchAndCutParameters::CallType::Exact;
-    }
 };
 
 }
